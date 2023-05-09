8cab0cca3d204178284bc6ffe986e901da3a06d5
Henil Valand <95482555+henilvaland@users.noreply.github.com>
2023-05-09 16:35:04 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,179 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { Button } from "../styles/Button";
+import { FaDiscord, FaInstagram, FaYoutube } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <>
+      <Wrapper>
+        <section className="contact-short">
+          <div className="grid grid-two-column">
+            <div>
+              <h3>Ready to get started?</h3>
+              <h3>Talk to us today</h3>
+            </div>
+
+            <div>
+              <Button className="btn hireme-btn">
+                <NavLink to="/"> Get Started </NavLink>
+              </Button>
+            </div>
+          </div>
+        </section>
+        {/* footer section */}
+
+        <footer>
+          <div className="container grid grid-four-column">
+            <div className="footer-about">
+              <h3>Weary Watches India</h3>
+              <p>Gen-Z's way of Buying Exclusive Time-pieces </p>
+            </div>
+            <div className="footer-subscribe">
+              <h3>Subscribe to get important updates</h3>
+              <form action="#">
+                <input type="email" className="round con-email" name="email" placeholder="YOUR E-MAIL" />
+                  {/* <button className="round-con">Email</button> */}
+                {/* <input type="submit" value="subscribe" /> */}
+                <button className="round-con">SUBSCRIBE</button>
+              </form>
+            </div>
+            <div className="footer-social">
+              <h3>Follow Us</h3>
+              <div className="footer-social--icons">
+                <div>
+                  <FaDiscord className="icons" />
+                </div>
+                <div>
+                  <FaInstagram className="icons" />
+                </div>
+                <div>
+                    <FaYoutube className="icons" />
+                </div>
+              </div>
+            </div>
+            <div className="footer-contact">
+              <h3>Call Us</h3>
+              <h3>+91 9998474563</h3>
+            </div>
+          </div>
+
+          <div className="footer-bottom--section">
+            <hr />
+            <div className="container grid grid-two-column ">
+              <p>
+                @{new Date().getFullYear()} Weary Watches India . All Rights Reserved
+              </p>
+              <div>
+                <p>PRIVACY POLICY</p>
+                <p>TERMS & CONDITIONS</p>
+              </div>
+            </div>
+          </div>
+        </footer>
+      </Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.section`
+  .iSIFGq {
+    margin: 0;
+  }
+  .contact-short {
+    max-width: 60vw;
+    margin: auto;
+    padding: 5rem 10rem;
+    background-color: ${({ theme }) => theme.colors.bg};
+    border-radius: 1rem;
+    box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+    transform: translateY(50%);
+    .grid div:last-child {
+      justify-self: end;
+      align-self: center;
+    }
+  }
+  footer {
+    padding: 14rem 0 9rem 0;
+    background-color: ${({ theme }) => theme.colors.footer_bg};
+    h3 {
+      color: ${({ theme }) => theme.colors.hr};
+      margin-bottom: 2.4rem;
+    }
+    p {
+      color: ${({ theme }) => theme.colors.white};
+    }
+    .footer-social--icons {
+      display: flex;
+      gap: 2rem;
+      div {
+        padding: 1rem;
+        border-radius: 50%;
+        border: 2px solid ${({ theme }) => theme.colors.white};
+        .icons {
+          color: ${({ theme }) => theme.colors.white};
+          font-size: 2.4rem;
+          position: relative;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+  .footer-bottom--section {
+    padding-top: 9rem;
+    hr {
+      margin-bottom: 2rem;
+      color: ${({ theme }) => theme.colors.hr};
+      height: 0.1px;
+    }
+  }
+  .round-con{
+    position : relative;
+    top : 10px;
+    text-decoration: none;
+    max-width: auto;
+    background-color: rgb(98 84 243);
+    color: rgb(255 255 255);
+    padding: 1.4rem 2.4rem;
+    border: none;
+    border-radius: 8px;
+    text-transform: uppercase;
+    text-align: center;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    -webkit-transition: all 0.3s ease 0s;
+    -moz-transition: all 0.3s ease 0s;
+    -o-transition: all 0.3s ease 0s;
+    &:hover,
+    &:active {
+      box-shadow: 0 2rem 2rem 0 rgb(132 144 255 / 30%);
+      box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+      transform: scale(0.96);
+    }
+    a {
+      text-decoration: none;
+      color: rgb(255 255 255);
+      font-size: 1.8rem;
+    }
+  }
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .contact-short {
+      max-width: 80vw;
+      margin: 4.8rem auto;
+      transform: translateY(0%);
+      text-align: center;
+      .grid div:last-child {
+        justify-self: center;
+      }
+    }
+    footer {
+      padding: 9rem 0 9rem 0;
+    }
+    .footer-bottom--section {
+      padding-top: 4.8rem;
+    }
+  }
+`;
+
+export default Footer;
